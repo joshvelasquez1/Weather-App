cda77a68ff33644a7ab11db1d953b8b3a75ed2aa
joshvelasquez1 <114311287+joshvelasquez1@users.noreply.github.com>
2022-09-24 15:14:10 -0500

    Create WeatherApp.py

---
+++ WeatherApp.py
@@ -0,0 +1,20 @@
+#obtain weather data
+#welcome zone
+	#display welcome ask for input
+	#get zipcode or city name
+	#check input for validity
+		#if valid
+			#store zipcode or city name
+		#else 
+			#inform data was not valid
+			#return to start
+#weather display
+	#compare input to data find match
+	#display data
+	#ask check another city? y for yes n for no
+		#if y
+			#back to welcome zone
+		#if n
+			#quit
+		#else 
+			#not valid data
