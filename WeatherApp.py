46eb5e48bfcf1c56394fabe7b45ed0b707cbac12
joshvelasquez1 <114311287+joshvelasquez1@users.noreply.github.com>
2022-10-08 20:17:29 -0500

    Update WeatherApp.py

--- WeatherApp.py
+++ WeatherApp.py
@@ -1,20 +1,46 @@
-#obtain weather data
-#welcome zone
-	#display welcome ask for input
-	#get zipcode or city name
-	#check input for validity
-		#if valid
-			#store zipcode or city name
-		#else 
-			#inform data was not valid
-			#return to start
-#weather display
-	#compare input to data find match
-	#display data
-	#ask check another city? y for yes n for no
-		#if y
-			#back to welcome zone
-		#if n
-			#quit
-		#else 
-			#not valid data
+import json, requests
+#functions for weather
+def temp():
+	temp = unformated_data["main"]["temp"]
+	print(f"The current temperature is: {temp}")
+
+def temp_min():
+	temp_min = unformated_data["main"]["temp_min"]
+	print(f"The minimum temperature is: {temp_min}")
+
+def temp_max():
+	temp_max = unformated_data["main"]["temp_max"]
+	print(f"The maximum temperature is: {temp_max}")
+
+def weather_main():
+	weather_main = unformated_data["weather"][0]["main"]
+	print(f"The weather looks like: {unformated_data['weather'][0]['main']}")
+
+
+while True:
+	print("Welcome! Please enter your ZIP Code or City Name to see the temperature or type quit to quit!")
+
+	#URL building components
+	base_url = "https://api.openweathermap.org/data/2.5/weather"
+	appid = "7be3619db7fadee15790446e998a140f"
+	location = input()
+
+	if location == "quit": 
+		print("Goodbye!")
+		break
+	else:
+		url = f"{base_url}?q={location}&units=imperial&APPID={appid}"
+		response = requests.get(url)
+		unformated_data = response.json()
+		if unformated_data == {"cod":"404","message":"city not found"}:
+			print()
+			print("Sorry the entered information is incorrect")
+			print()
+		else:
+			temp()
+			temp_min()
+			temp_max()
+			weather_main()
+			print()
+
+# key: 7be3619db7fadee15790446e998a140f
